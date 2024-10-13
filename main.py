8efc51abe4ae2f751b9bebc1e8f8973b775bc9f2
WillieApfel <50503962+WillieApfel@users.noreply.github.com>
2024-10-13 10:58:34 -0400

    Actualizar main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ for file in files:
         players_list = players_df['player.id'].unique().tolist()
         players_df = pd.concat([players_df, df.loc[~df['player.id'].isin(players_list)]], ignore_index=True)
    
-player_options, teams = players_breakdown(players_df.sort_values(by=['player.lastName','season'], ascending=[True, False]))
+player_options, teams = players_breakdown(players_df.sort_values(by=['player.lastName']).sort_values(by=['season'], ascending=[False]))
 
 _dir = './Static/Data/Stats/'
 files = os.listdir(_dir)
